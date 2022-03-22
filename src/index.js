35ad281e1ec101c698e16fc9370d8bbea7b1c615
Augusto <jesus_augga_99@icloud.com>
2022-03-22 10:22:14 -0500

    add css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import './style.css';
 import printMe from './print.js';
 
 function component() {
@@ -6,6 +7,7 @@ function component() {
   const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,20 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: './src/index.html'
     }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
